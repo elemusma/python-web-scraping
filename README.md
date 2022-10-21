5e0790d015c8e95a7e95de65229455751baa81c1
Efrain Lemus <efraintlemusm@gmail.com>
2022-10-20 21:44:25 -0600

    get title and date from seeking alpha rss feed

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # python-web-scraping
 
-Try this link:
-https://stackoverflow.com/questions/60809986/access-denied-while-scraping-seekingalpha-transcript-with-beautifulsoup
+# Try this link:
+# https://stackoverflow.com/questions/60809986/access-denied-while-scraping-seekingalpha-transcript-with-beautifulsoup
+
+# Advanced GitHub Project:
+# https://github.com/jamesjynus/Web_Scraping_Project
--- websamed.py
+++ websamed.py
@@ -1,19 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://seekingalpha.com/market-news'
+# url = 'https://brownstocktrading.com/'
+url = 'https://seekingalpha.com/api/sa/combined/TSLA.xml'
 # url = 'https://seekingalpha.com/symbol/TSLA/analysis'
 # url = 'https://seeking-alpha.p.rapidapi.com/analysis/TSLA/'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 
-titles = soup.select('a.qfE')
+# titles = soup.select('a.qfE')
+# titles = soup.select('.wp-block-group .wp-block-post-title a')
+titles = soup.select('item title') # seeking alpha rss feed
+dates = soup.select('item pubDate') # seeking alpha rss feed
+# titles = soup.select('.zcA.keA .ruA.bkA .ruI .ruB .ruE') # seeking alpha actual analysis page
 
-titlesAll = titles[:10]
+titlesAll = titles[:]
+datesAll = dates[:]
 
-for anchor in titlesAll:
-    print('hello')
-    print(anchor.text)
+# for anchor in titlesAll:
+#     # print('hello')
+#     print(anchor.text)
+
+for title, date in zip(titlesAll, datesAll):
+    print('Title: ' + title.text + ' - Published on: ' + date.text)
 
 
 # print(titles)
